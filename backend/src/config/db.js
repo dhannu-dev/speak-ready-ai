e147af3ad7ff351059ce8bf8ef3cc899609a7bf2
dhannu-dev <krdhanak47@gmail.com>
2026-06-06 18:19:27 +0530

    backend setup utils

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,9 @@ export const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
+    console.log(
+      `\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("Mongodb connection error", error);
     process.exit(1);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,19 @@
-import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
 import { connectDB } from "./config/db.js";
+import app from "./app.js";
 
 dotenv.config();
-
-connectDB();
-
-const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+connectDB()
+.then(() => {
+   app.listen(PORT, () => {
+    console.log(`server is running on PORT : ${PORT}`)
+   })
+})
+.catch((error) => {
+  console.log("Mongodb connection failed", error)
+})
+
+
+ 

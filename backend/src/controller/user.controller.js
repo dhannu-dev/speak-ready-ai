116f7884b43a42c03f307873f660ab8ffdc983e1
dhannu-dev <krdhanak47@gmail.com>
2026-06-07 17:32:53 +0530

    make register api

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,7 +1,40 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import ApiResponse from "../utils/apiResponse.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: "ok",
+  //Check the field
+  //Check User is already exits?
+  //hashpassword
+  //create user object
+  // return res
+
+  const { name, email, password } = req.body;
+  if ([name, email, password].some((field) => field.trim() === "")) {
+    throw new ApiError(400, "All field is required");
+  }
+
+  const exitingUser = await User.findOne({ $or: [{ email }] });
+  if (exitingUser) {
+    throw new ApiError(409, "User Already Exits");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashPassword,
   });
+
+  if (!user) {
+    throw new ApiError(500, "user not created");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, user, "user created successfully"));
 });

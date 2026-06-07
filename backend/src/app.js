f3dfbbf508b550085a7ddf72f31eb3fd0cc5924c
dhannu-dev <krdhanak47@gmail.com>
2026-06-07 16:02:58 +0530

    make the user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,14 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true , limit : "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
+
+// import router
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { registerUser } from "../controller/user.controller.js";
+const router = Router();
+
+router.post("/register", registerUser);
+
+export default router;

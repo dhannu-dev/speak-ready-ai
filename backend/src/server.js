180ace96040cf02d61266839b7d3a2bb41f317f0
dhannu-dev <krdhanak47@gmail.com>
2026-06-08 21:38:31 +0530

    making the folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,21 @@
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
+import ApiResponse from "./utils/apiResponse.js";
 
 dotenv.config();
 const PORT = Number(process.env.PORT) || 5000;
 
+app.get("/testing", (req, res) => {
+  return res.status(201).json(new ApiResponse(200, "Hello dhannu"));
+});
+
 connectDB()
-.then(() => {
-   app.listen(PORT, () => {
-    console.log(`server is running on PORT : ${PORT}`)
-   })
-})
-.catch((error) => {
-  console.log("Mongodb connection failed", error)
-})
-
-
- 
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on PORT : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed", error);
+  });
